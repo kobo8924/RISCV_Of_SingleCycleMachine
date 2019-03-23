b2e1ac8cbe2054945f7e09a4b0513a9a30d992f6
k0b0 <kobo8924@gmail.com>
2019-03-23 11:47:16 +0900

    2019/03/23

--- rtl/DataMemory.sv
+++ rtl/DataMemory.sv
@@ -10,7 +10,10 @@
 // Name          I/O   SIZE   props
 // ================================================
 // clk             I      1   clock
-//
+// MemWrite        I      1   WriteEnable
+// Address         I     64
+// Writedata       I     64
+// Readdata        O     64
 // ================================================
 // 
 //
@@ -25,7 +28,7 @@ module DataMemory (input                        clk,
 	           input  [`DATA_W-1:0]         Writedata,
 	           output [`DATA_W-1:0]         Readdata);
 
-//配列数を2のべき乗にするとdmemからデータが読み出せなくなるので、配列数を減ら
+//配列数を2のべき乗にすると、なぜかdmemからデータが読み出せなくなるので、配列数を減ら
 //している
 //logic [`DATA_W-1:0] dmem [2**`DATA_W-1:0] ;
 logic [`DATA_W-1:0] dmem [0:31] ;
@@ -35,7 +38,6 @@ initial
 begin
         $readmemh(`DMEM_FILE_PATH, dmem);
 end
-
 
 
 always_ff @(posedge clk)
